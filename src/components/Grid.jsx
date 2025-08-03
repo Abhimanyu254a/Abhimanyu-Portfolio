ca65b5cd255258561c3d59e7677dbf8e0c2fb9a3
Abhimanyu Sharma <abhimanyu254a@gmail.com>
2025-08-04 03:38:39 +0530

    Done Some Work in the Grid

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -11,16 +11,22 @@ function Grid()
         <div className="Container">
         <div className="top">
         <GridBlock  ClassName='large-top' desciption={'About me'}/>
-        <GridBlock  ClassName='middle' desciption={'Projects'}/>
-        <GridBlock  ClassName='small' desciption={'Github_Linkdin'}/>
-        <GridBlock ClassName='small' desciption={'Contact_Me'}/>
-        </div> 
-        <GridBlock ClassName='center' desciption={'Me'}/>
+        
+        <GridBlock ClassName='small-bottom' desciption={'Tech_Stack'}/> 
+        <GridBlock ClassName='small-bottom' desciption={'Certication'}/> 
+        </div>
+        <div className="center">
+            <GridBlock  ClassName='middle-top' desciption={'Projects'}/>
+            <GridBlock ClassName='middle' desciption={'Me'}/>
+            <GridBlock ClassName='middle-bottom' desciption={'Achievement'}/> 
+            </div> 
+        
         <div className="bottom">
 
-        <GridBlock ClassName='small' desciption={'Tech_Stack'}/> 
-        <GridBlock ClassName='small' desciption={'Certication'}/> 
-        <GridBlock ClassName='middle' desciption={'Achievement'}/> 
+        <GridBlock  ClassName='small-top' desciption={'G & L'}/>
+        <GridBlock ClassName='small-top' desciption={'Contact'}/>
+ 
+
         <GridBlock ClassName='large-bottom' desciption={'Resume'}/> 
         </div>
         </div>
